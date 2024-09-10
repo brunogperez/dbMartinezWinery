4d866c7294a5ea2882ffc7b9b975105e913b3dba
Bruno Perez <brunogperez91@gmail.com>
2024-09-10 16:49:27 -0300

    Update Script
    Added some views and functions.

--- martinezWinery.sql
+++ martinezWinery.sql
@@ -46,6 +46,8 @@ CREATE TABLE products (
     price DECIMAL NOT NULL,
     category_id INT NOT NULL,
     year_id INT NOT NULL,
+    provider_id INT,
+    FOREIGN KEY(provider_id) REFERENCES providers(provider_id),
     FOREIGN KEY(year_id) REFERENCES years(year_id),
     FOREIGN KEY(category_id) REFERENCES categories(category_id)
     );
@@ -93,6 +95,77 @@ CREATE TABLE orders (
     FOREIGN KEY(client_id) REFERENCES clients(client_id),
     FOREIGN KEY(status_id) REFERENCES status(status_id)
     );
+    
+-- Tabla de detalle de Pedido
+CREATE TABLE orders_detail (
+	orderdetail_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	order_id INT NOT NULL,
+    product_id INT NOT NULL, 
+    quantity INT,
+    amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY(product_id) REFERENCES products(product_id),
+    FOREIGN KEY(order_id) REFERENCES orders(order_id)
+    );
+    	
+        
+        
+-- Creación de vistas para uso en la DB
+
+-- Vista para observar la cantidad de pedidos de un cliente en específico
+CREATE OR REPLACE VIEW ClientOrderCount AS
+SELECT c.client_id,
+       c.name,
+       COUNT(o.order_id) AS total_orders
+FROM clients c
+LEFT JOIN orders o ON c.client_id = o.client_id
+GROUP BY c.client_id, c.name;
 
 
-	
+-- Vista para observar los productos con poco stock
+CREATE VIEW ProductsWithLowStock AS
+SELECT product_id,
+       name,
+       stock
+FROM products
+WHERE stock <= 10;
+
+-- Vista de stock de productos por marca, categoría y año
+CREATE VIEW StockByBrandCategoryAndYear AS
+SELECT p.year,
+       b.name,
+       c.name,
+       SUM(p.stock) AS total_stock
+FROM products p
+JOIN categories c ON p.category_id = c.category_id
+JOIN providers b ON p.providers_id = b.provider_id
+GROUP BY p.year, b.name, c.name
+ORDER BY p.year, b.name, c.name;
+
+-- Vista de pedidos incompletos
+
+CREATE VIEW IncompleteOrders AS
+SELECT o.order_id,
+       o.client_id,
+       o.order_date
+FROM orders o
+WHERE o.status = 'pending';
+
+
+        
+-- Declaración de funciones para la DB
+
+-- Función para obtener el precio total de un pedido
+DELIMITER //
+CREATE FUNCTION GetTotalPrice(order_id INT)
+RETURNS DECIMAL(10, 2)
+BEGIN
+    DECLARE total_price DECIMAL(10, 2);
+    
+    SELECT SUM(p.price * o.quantity) INTO total_price
+    FROM orders o
+    JOIN products p ON o.product_id = p.product_id
+    WHERE o.order_id = order_id;
+    
+    RETURN total_price;
+END;
+//
